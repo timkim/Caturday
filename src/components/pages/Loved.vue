090bf67058c390f81d3c72f16a9d2b23da574e48
timkim <timk@adobe.com>
2017-04-06 16:42:21 -0700

    Add in loved page and fixed up styles there

---
+++ src/components/pages/Loved.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul>
+    <li class="cat-container" v-for="item in items" v-bind:style="catItemStyle">
+      <Cat v-bind:message=item.src></Cat>
+    </li>
+  </ul>
+</template>
+
+<script>
+  import Cat from './Cat';
+
+  export default {
+    name: 'Loved',
+    components: {
+      'Cat': Cat
+    },
+    data () {
+      return {
+        title: 'Loved',
+        catItemStyle: {
+          listStyle: 'none'
+        },
+        items: this.$store.getters.getLovedCats.lovedCats
+      };
+    },
+    mounted () {
+    }
+  };
+</script>
