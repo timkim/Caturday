50f7dd31a0bc25b843d61be5183f5f7ac5134bd9
timkim <timk@adobe.com>
2017-04-10 16:46:38 -0700

    Updated about page

--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -1,19 +1,23 @@
 <template>
   <div>
     <!-- Scrollable page content-->
-    <f7-block-title>{{ title }}</f7-block-title>
     <f7-block inner>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-      Distinctio est aliquam officiis quaerat placeat, cum explicabo magni
-      soluta totam maxime autem minima accusamus eos suscipit dignissimos
-      corporis modi voluptatum fugiat!
+      This app is based off of the <f7-link href="https://github.com/phonegap/phonegap-template-vue-f7-tabs" external>phonegap-template-vue-f7-tabs template</f7-link>. Feel free to fork, modify, or do as you wish with the app. MIT License.
+
+      <br />
+      <br />
+      Coded and written by Tim Kim.
+
+      <br />
+      @timkim on <f7-link href="http://github.com/timkim" external>github</f7-link>, <f7-link href="http://twitter.com/timkim" external>twitter</f7-link> and elsewhere.
+
     </f7-block>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'Services',
+    name: 'About',
     data () {
       return {
         title: 'About'
